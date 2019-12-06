35ba67f6f81926cca6879a8a3a5ddb9d909d1bc6
Dom Clemmer <domclemmer@Doms-MacBook-Air.local>
2019-12-05 23:16:07 -0500

    create base app router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Layout from './components/hoc/Layout'
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppRouter = () => (
+  <Layout>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <h1>home</h1>
+        </Route>
+        <Route path="/about">
+          <h1>about</h1>
+        </Route>
+      </Switch>
+    </Router>
+  </Layout>
+)
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
